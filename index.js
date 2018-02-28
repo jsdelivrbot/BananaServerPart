0aec45756b40defd6e7daab0caac0687cb8e90df
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-02-28 19:52:41 +0400

    commit3

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const http = require('http');
-/*
+
 var server=http.createServer(function(req,res){
   res.writeHead(200);
   res.end("Banana 555");
-}).listen(555);*/ 
+}).listen(555);
 
  
 express()
