72665bdc0fe963d51d31bf60add3d584ac3df478
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-02-28 19:58:10 +0400

    commit4

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const http = require('http');
 
-var server=http.createServer(function(req,res){
+http.createServer(function(req,res){
   res.writeHead(200);
   res.end("Banana 555");
-}).listen(555);
+
 
  
 express()
@@ -14,4 +14,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+}).listen(555);
