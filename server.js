4502eecd65d68f4f24c2be9475d1364bb1cce8ac
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-01 16:50:52 +0400

    commit31

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express')();
-//var http = require('http');
+//var express = require('express');
+var http = require('http');
 
-express.get('/',function(req,res){
+/*express.get('/',function(req,res){
 	res.send('Hellow');
-})
-/*
+})*/
+
 http.listen(4321,function(){
 	console.log('listen');
-})*/
+})
