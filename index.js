e2b2d083ec9f0c28f953095416267b3c77d22b37
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-01 14:58:04 +0400

    commit12

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5555;//process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
