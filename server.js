7b3f390c7b6176b62fba80a0198930b4815448f6
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-01 16:37:23 +0400

    commit31

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
 var express = require('express');
-var cool = require('cool-ascii-faces');
-var app = express();
+var http = require('http');
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+express.get('/',function(req,res){
+	res.send('Hellow');
+})
 
-// set the view engine to ejs
-app.set('view engine', 'html');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-	// ejs render automatically looks in the views folder
-	res.render('index');
-});
-app.get('/cool', function(request, response) {
-	response.send(cool());
-});
-app.listen(port, function() {
-	console.log('Our app is running on http://localhost:' + port);
-});
+http.listen(4321,function(){
+	console.log('listen');
+})
