cad8479968401139edd9947f1ed62cc62a5516b2
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-01 16:52:49 +0400

    commit31

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var http = require('http');
 	res.send('Hellow');
 })*/
 
-http.listen(4321,function(){
-	console.log('listen');
-})
+var server = http.createServer((req, res) => {
+		res.writeHead(200, {'Content-type':'text/html'});
+res.end('<h1>Hello NodeJS</h1>');
+});
+
+server.listen(4321,() => console.log('Server running on port 3000'));
