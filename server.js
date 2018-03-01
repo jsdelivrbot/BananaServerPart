e8e12562374c7ec553fd7c7d3d8a8bea7d5babb7
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-01 15:21:59 +0400

    commit17

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
@@ -6,13 +8,14 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-		.use((req, res) => res.sendFile(INDEX) ).listen(PORT, () => console.log('Listening on ${ PORT }'));
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
-	console.log('Client connected');
-socket.on('disconnect', () => console.log('Client disconnected'));
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
