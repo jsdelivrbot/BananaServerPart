790c8a17289ee90e560d31ca66a9fa1e1be86cc2
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-01 16:12:52 +0400

    commit31

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
 // set the home page route
 app.get('/', function(req, res) {
