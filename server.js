833ddce6829d884d1b4b7aa211d3340b5b2d61bf
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-02 17:42:05 +0400

    commit6

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-var io = require('socket.io').listen(8080);
-// Отключаем вывод полного лога - пригодится в production'е
-io.set('log level', 1);
-// Навешиваем обработчик на подключение нового клиента
-io.sockets.on('connection', function (socket) {
-	console.log("connect");
-	// Т.к. чат простой - в качестве ников пока используем первые 5 символов от ID сокета
-	var ID = (socket.id).toString().substr(0, 5);
-	var time = (new Date).toLocaleTimeString();
-	// Посылаем клиенту сообщение о том, что он успешно подключился и его имя
-	socket.json.send({'event': 'connected', 'name': ID, 'time': time});
-	// Посылаем всем остальным пользователям, что подключился новый клиент и его имя
-	socket.broadcast.json.send({'event': 'userJoined', 'name': ID, 'time': time});
-	// Навешиваем обработчик на входящее сообщение
-	socket.on('message', function (msg) {
-		var time = (new Date).toLocaleTimeString();
-		// Уведомляем клиента, что его сообщение успешно дошло до сервера
-		socket.json.send({'event': 'messageSent', 'name': ID, 'text': msg, 'time': time});
-		// Отсылаем сообщение остальным участникам чата
-		socket.broadcast.json.send({'event': 'messageReceived', 'name': ID, 'text': msg, 'time': time})
+var app=require('http').createServer(handler).listen(5050);
+var io =require('socket.io').listen(app);
+var fs = require('fs');
+
+
+function handler(req,res){
+	fs.readFile(__dirname+"index.html",function(err,data){
+		if(err){
+			res.writeHead(500);
+			return res.end("Err load");
+		}
+		res.writeHead(200);
+		res.end(data);
 	});
-	// При отключении клиента - уведомляем остальных
-	socket.on('disconnect', function() {
-		var time = (new Date).toLocaleTimeString();
-		io.sockets.json.send({'event': 'userSplit', 'name': ID, 'time': time});
+}
+
+io.sockets.on('connection',function(socket){
+	socket.on('addme',function(user){
+		socket.username=user;
+		socket.emit('chat','Server','Connected');
+		socket.broadcast.emit('chat','Server',user + 'on deck');
+
+	});
+	socket.on('sendchat',function(data){
+		io.sockets.emit('chat',socket.username,data);
+
+	});
+	socket.on('disconect',function(){
+		io.sockets.emit('chat','Server',socket.username + 'left');
 	});
 });
