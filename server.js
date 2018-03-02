03f9cd55c92fcb858668536ae5073bd1741812ba
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-02 18:54:57 +0400

    commit6

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var app=require('http').createServer(handler).listen(80);
+var app=require('http').createServer(handler).listen(8080);
 var io =require('socket.io').listen(app);
 var fs = require('fs');
 
