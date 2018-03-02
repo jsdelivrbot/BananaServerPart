3e16df2aa8dcd24f0ae25ebf5806277773735b19
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-02 19:12:18 +0400

    commit6

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
   const express = require('express');
-  const socketIO = require('socket.io');
   const path = require('path');
 
   const PORT = process.env.PORT || 3000;
@@ -9,12 +8,4 @@
   const server = express()
     .use((req, res) => res.sendFile(INDEX)  )
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-  const io = socketIO(server);
-
-  io.on('connection', (socket) => {
-    console.log('Client connected');
-    socket.on('disconnect', () => console.log('Client disconnected'));
-  });
-
-  setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+  
