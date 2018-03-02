b3f6d1746247ea67eac134346d764a502a0e97e6
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-02 19:50:43 +0400

    commit6

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
@@ -8,6 +10,8 @@ const INDEX = path.join(__dirname, 'index.html');
 const server = express()
 		.use((req, res) => res.sendFile(INDEX) )
 .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
 
 io.sockets.on('connection',function(socket){
 	socket.on('addme',function(user){
