91a7e994e5a3679b16f734894a0f787173275fad
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-14 15:01:50 +0400

    commit6

--- controllers/start.js
+++ controllers/start.js
@@ -2,10 +2,10 @@ var start = require("../models/start_module.js");
 
 module.exports.controller = function (app) {
     app.get('/start', function (req, res) {
-        res.render('start/on');
+        res.render('start');
     });
 
     app.get('/end', function (req, res) {
-        res.render('end/on');
+        res.render('end');
     });
 }
