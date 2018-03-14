54e0c789b9553d0bec40609dcaf2326514216ec2
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-14 14:22:06 +0400

    commit6

---
+++ models/start_module.js
@@ -0,0 +1,11 @@
+var start = require("../modules/start.js");
+
+module.exports.controller = function (app) {
+    app.get('/start', function (req, res) {
+        res.render('start/on');
+    });
+
+    app.get('/end', function (req, res) {
+        res.render('end/on');
+    });
+}
