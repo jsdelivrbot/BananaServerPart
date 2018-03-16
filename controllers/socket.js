1319eb2355f43d47197284803bf2ce3aa976d9a9
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-16 15:54:56 +0400

    commited

--- controllers/socket.js
+++ controllers/socket.js
@@ -2,28 +2,26 @@
 
 exports.Socket = function (request, response) {
 	response.render('socket', { title: 'Socket', message: 'MVC Socket!' });
-    var io = socketIO();
-
-    io.sockets.on('connection', function (socket) {
+      io.sockets.on('connection', function (socket) {
         socket.on('addme', function (user) {
-            socket.username = user;
-            socket.emit('chat', 'Server Connected');
-            socket.broadcast.emit('chat', 'Server ' + user + ' on deck');
+          socket.username = user;
+          socket.emit('chat', 'Server Connected');
+          socket.broadcast.emit('chat', 'Server ' + user + ' on deck');
 
         });
         socket.on('sendchat', function (data) {
-            io.sockets.emit('chat', socket.username + ":" + data);
+          io.sockets.emit('chat', socket.username + ":" + data);
 
         });
 
         socket.on('jsoncreater', function (json) {
-            io.sockets.emit("middle", json);
-            socket.broadcast.emit("chat", json);
+          io.sockets.emit("middle", json);
+          socket.broadcast.emit("chat", json);
         });
         socket.on('disconect', function () {
-            io.sockets.emit('chat', 'Server', socket.username + 'left');
+          io.sockets.emit('chat', 'Server', socket.username + 'left');
         });
-    });
+      });
 };
 
 
--- index.js
+++ index.js
@@ -41,23 +41,3 @@ fs.readdirSync('./models').forEach(function(file){
 
 
 
-io.sockets.on('connection', function (socket) {
-	socket.on('addme', function (user) {
-		socket.username = user;
-		socket.emit('chat', 'Server Connected');
-		socket.broadcast.emit('chat', 'Server ' + user + ' on deck');
-
-	});
-	socket.on('sendchat', function (data) {
-		io.sockets.emit('chat', socket.username + ":" + data);
-
-	});
-
-	socket.on('jsoncreater', function (json) {
-		io.sockets.emit("middle", json);
-		socket.broadcast.emit("chat", json);
-	});
-	socket.on('disconect', function () {
-		io.sockets.emit('chat', 'Server', socket.username + 'left');
-	});
-});
