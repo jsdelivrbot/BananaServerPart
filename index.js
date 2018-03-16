5f3796ac07cc1494ad0916a80e5f438cad22c477
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-16 16:40:30 +0400

    commited

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-
-
 var express = require('express');
-
-
 var path     = require('path');
 var fs = require('fs');
-
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 
+var app = require('http').createServer(handler);
+var Socketio = require('socket.io').listen(app);
+app.listen(8124);
+
+function handler (req, res) {
+	fs.readFile(__dirname + '/index.html',
+		function (err, data) {
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading index.html');
+			}
+			res.writeHead(200);
+			res.end(data);
+		});
+}
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
-var io = require('socket.io');
+
 
 
 var server = express();
@@ -38,8 +48,7 @@ fs.readdirSync('./models').forEach(function(file){
 	}
 })
 
-var http = require('http').Server(server);
-var Socketio = io(http);
+
 
 Socketio.sockets.on('connection', function (socket) {
 	console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
