09a28c6ff4e5a5e0e96903b25c5580e14025e158
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-16 15:51:29 +0400

    commited

--- index.js
+++ index.js
@@ -39,3 +39,25 @@ fs.readdirSync('./models').forEach(function(file){
 	}
 })
 
+var io = socketIO();
+
+io.sockets.on('connection', function (socket) {
+	socket.on('addme', function (user) {
+		socket.username = user;
+		socket.emit('chat', 'Server Connected');
+		socket.broadcast.emit('chat', 'Server ' + user + ' on deck');
+
+	});
+	socket.on('sendchat', function (data) {
+		io.sockets.emit('chat', socket.username + ":" + data);
+
+	});
+
+	socket.on('jsoncreater', function (json) {
+		io.sockets.emit("middle", json);
+		socket.broadcast.emit("chat", json);
+	});
+	socket.on('disconect', function () {
+		io.sockets.emit('chat', 'Server', socket.username + 'left');
+	});
+});
