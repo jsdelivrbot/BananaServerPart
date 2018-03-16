1be42ec15e922b985fd84fa1fe7c2816846d2a44
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-16 17:13:28 +0400

    commited

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var express = require('express')();
+var app = require('express')();
 var path     = require('path');
 var fs = require('fs');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
-var server = require('http').Server(express);
+var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 const PORT = process.env.PORT || 3000;
@@ -13,15 +13,15 @@ const INDEX = path.join(__dirname, 'index.html');
 
 //server.use((req, res) => res.sendFile(INDEX));
 
-server.set('port', process.env.PORT || 3000);
-server.set('views', path.join(__dirname, 'views'));
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
 
-server.set('view engine', 'jade');
-server.set('view options', { layout: 'layout' });
-server.use(methodOverride());
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.set('view options', { layout: 'layout' });
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(path.join(__dirname, 'views'));
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
@@ -29,7 +29,7 @@ fs.readdirSync('./models').forEach(function(file){
     if (file.substr(-3) == '.js') {
 
 		var route = require('./models/'+file);
-        route(server);
+        route(app);
 	}
 })
 
