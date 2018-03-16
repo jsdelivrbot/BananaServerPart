d3fdeb697364bae00005f3825962ca7ab4e3a9d4
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-16 14:52:14 +0400

    commited

--- models/start.js
+++ models/start.js
@@ -1,8 +1,10 @@
 
 var Start = require('../controllers/start');
+var Socket = require('../controllers/socket');
 
 module.exports = function (app) {
     app.get('/', Start.Index);
 
     app.get('/start', Start.Start);
+    app.get('/socket.io', Socket.Socket);
 }
