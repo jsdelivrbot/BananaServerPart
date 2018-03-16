d029cb6135f2e59cac6248a4573b9346ed281550
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-16 16:00:35 +0400

    commited

--- controllers/socket.js
+++ controllers/socket.js
@@ -2,26 +2,7 @@
 
 exports.Socket = function (request, response) {
 	response.render('socket', { title: 'Socket', message: 'MVC Socket!' });
-	global.io.sockets.on('connection', function (socket) {
-        socket.on('addme', function (user) {
-          socket.username = user;
-          socket.emit('chat', 'Server Connected');
-          socket.broadcast.emit('chat', 'Server ' + user + ' on deck');
 
-        });
-        socket.on('sendchat', function (data) {
-	        global.io.sockets.emit('chat', socket.username + ":" + data);
-
-        });
-
-        socket.on('jsoncreater', function (json) {
-	        global.io.sockets.emit("middle", json);
-          socket.broadcast.emit("chat", json);
-        });
-        socket.on('disconect', function () {
-	        global.io.sockets.emit('chat', 'Server', socket.username + 'left');
-        });
-      });
 };
 
 
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const INDEX = path.join(__dirname, 'index.html');
 
 var server = express();
 var http = require('http').Server(server);
-global.io = require('socket.io')(http);
+var io = require('socket.io')(http);
 //server.use((req, res) => res.sendFile(INDEX));
 
 server.set('port', process.env.PORT || 3000);
@@ -40,4 +40,23 @@ fs.readdirSync('./models').forEach(function(file){
 })
 
 
+io.sockets.on('connection', function (socket) {
+	socket.on('addme', function (user) {
+		socket.username = user;
+		socket.emit('chat', 'Server Connected');
+		socket.broadcast.emit('chat', 'Server ' + user + ' on deck');
 
+	});
+	socket.on('sendchat', function (data) {
+		io.sockets.emit('chat', socket.username + ":" + data);
+
+	});
+
+	socket.on('jsoncreater', function (json) {
+		io.sockets.emit("middle", json);
+		socket.broadcast.emit("chat", json);
+	});
+	socket.on('disconect', function () {
+		io.sockets.emit('chat', 'Server', socket.username + 'left');
+	});
+});
