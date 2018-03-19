83ec4fcd361df862140403bcfdb5cd6bff5527f5
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-19 12:55:40 +0400

    commited

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ io.sockets.on('connection', function (socket) {
 	socket.on('addme', function (user) {
 
 		socket.username = user;
-		io.sockets.emit('chat', 'Server ' + user + ' on deck');
+		socket.broadcast.emit('chat', 'Server ' + user + ' on deck');
 		console.log('Server ' + user + ' on deck');
 
 	});
