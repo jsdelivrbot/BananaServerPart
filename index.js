11691074147bec48f847eac458fbc1e28d8c024b
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-19 15:31:48 +0400

    commited

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var path     = require('path');
 var fs = require('fs');
-var mongodb = require('mongodb').MongoClient;;
+var mongodb = require('mongodb');
 var bodyParser = require('body-parser');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -9,15 +9,8 @@ var io = require('socket.io')(server);
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
-/*var DBserver = new mongodb.Server('mongodb://ancient-gorge-52214.herokuapp.com',27017, {auto_reconnect: true});
-var db = new mongodb.Db('banan', DBserver);*/
-mongodb.connect("mongodb://ancient-gorge-52214.herokuapp.com:27017/banandb", function(err, db) {
-	if(!err) {
-		console.log("We are connected!!!");
-	}else{
-		console.log(err);
-	}
-});
+
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 
@@ -25,6 +18,20 @@ app.set('view engine', 'jade');
 app.set('view options', { layout: 'layout' });
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+var DBserver = new mongodb.Server('http://ancient-gorge-52214.herokuapp.com',27017, {auto_reconnect: true});
+var db = new mongodb.Db('banan', DBserver);
+
+db.open(function(err, db) {
+	if(!err) {
+		db.collection('bananData',function(err,result){
+			if(!err){
+				var coll={user:'123',data:'1254'};
+				result.insert(coll);
+			}
+			db.close();
+		})
+	}
+})
 
 fs.readdirSync('./models').forEach(function(file){
     if (file.substr(-3) == '.js') {
@@ -50,17 +57,7 @@ io.sockets.on('connection', function (socket) {
 
 	socket.on('jsoncreater', function (json) {
 
-		db.open(function(err, db) {
-			if(!err) {
-				db.collection('bananData',function(err,result){
-					if(!err){
-						var coll={user:socket.username,data:json};
-						result.insert(coll);
-					}
-					db.close();
-				})
-			}
-		})
+
 
 
 		io.sockets.emit("middle", socket.username+" Send: "+json);
