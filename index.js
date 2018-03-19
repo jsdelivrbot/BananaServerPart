bd187d025470c05332c24af8c44447785dace931
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-19 15:04:02 +0400

    commited

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var path     = require('path');
 var fs = require('fs');
-var methodOverride = require('method-override');
+var mongodb = require('mongodb');
 var bodyParser = require('body-parser');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -9,10 +9,8 @@ var io = require('socket.io')(server);
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
-//var server = express;
-
-//server.use((req, res) => res.sendFile(INDEX));
-
+var DBserver = new mongodb.Server('localhost',:27017, {auto_reconnect: true});
+var db = new mongdb.Db('banan', DBserver);
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 
@@ -44,6 +42,19 @@ io.sockets.on('connection', function (socket) {
 	});
 
 	socket.on('jsoncreater', function (json) {
+		db.open(function(err, db) {
+			if(!err) {
+				db.collection('bananData',function(err,result){
+					if(!err){
+						var coll={user:socket.username,data:json};
+						result.insert(coll);
+					}
+					db.close();
+				})
+			}
+		})
+
+
 		io.sockets.emit("middle", socket.username+" Send: "+json);
 	});
 	socket.on('disconect', function () {
