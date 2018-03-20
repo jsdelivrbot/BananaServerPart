7a0bc6ea97b68560041103922b0d817079de7f0c
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-03-20 12:10:25 +0400

    commited

--- models/start.js
+++ models/start.js
@@ -1,10 +1,12 @@
 
 var Start = require('../controllers/start');
 var Socket = require('../controllers/socket');
+var Chat = require('../controllers/chat');
 
 module.exports = function (app) {
     app.get('/', Start.Index);
 
     app.get('/start', Start.Start);
     app.get('/socket', Socket.Socket);
+    app.get('/chat', Chat.Chat);
 }
