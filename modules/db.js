18b3171408222cf23b62d30785f758d7bd144b51
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-04-02 13:43:20 +0400

    LoggerFix12

--- modules/db.js
+++ modules/db.js
@@ -19,8 +19,10 @@ exports.dbGetOne=function(table,data){
 		var collection = datas.collection(table);
 		var infos = JSON.parse(data);
 		inf=collection.findOne(infos);
+		inf.then(function(res){
+			console.log(res);
+		})
 		console.log("____________________");
-		console.log(inf);
 		db.close();
 	});
 
