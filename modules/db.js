3e83b2b1100b8e5999461b7dbaaab37c33c8b11b
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-04-02 15:19:52 +0400

    LoggerFix12

--- modules/db.js
+++ modules/db.js
@@ -1,5 +1,9 @@
 var MongoClient = require('mongodb').MongoClient;
 result=null;
+
+function setResult(res){
+	result=res;
+}
 exports.dbSend=function(table,data){
 
 	MongoClient.connect('mongodb://Singuliarity1:Qazxswedc1@ds215759.mlab.com:15759/banandata', function(err, db) {
@@ -20,7 +24,7 @@ exports.dbGetOne=function(table,data){
 		var infos = JSON.parse(data);
 		var inf=collection.findOne(infos,function(err,res) {
 			db.close();
-			result=res;
+			setResult(res);
 		});
 	});
 return result;
