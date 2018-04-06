5b981be008d33454fcf7704d6d0109c6c9c87215
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-04-06 14:47:22 +0400

    Lobyst

--- modules/agronom.js
+++ modules/agronom.js
@@ -9,17 +9,18 @@ exports.hireAgronom=function (socket,iosockets){
 			delete $agronomInfo["Hire_status"];
 			DB.dbUpdateOne("hireAgronom", $agronomInfo, $agronomHireStatus);
 			$datas = DB.dbGetOne("hireAgronom", data);
-
 			if ($datas != null) {
 				delete $datas["_id"];
 				delete $datas["Id_User"];
 				delete $datas["Id_map"];
 				delete $datas["Id_parsel"];
 				socket.emit('hireAgronom', $datas);
+			}else{
+				socket.emit('hireAgronom', "-1");
 			}
 		} else {
 			socket.emit('hireAgronom', "-1");
 		}
-	}
 	});
 }
+
