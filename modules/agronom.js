c2698b2e60a15696d8301c934289fafa5cc729c5
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-04-06 14:41:56 +0400

    Lobyst

--- modules/agronom.js
+++ modules/agronom.js
@@ -3,13 +3,22 @@ var DB=require("./db.js");
 
 exports.hireAgronom=function (socket,iosockets){
 	socket.on("hireAgronom",function(data){
-		$datas=DB.dbGetOne("hireAgronom",data);
-		if($datas!=null) {
-			delete $datas["_id"];
-			delete $datas["Id_User"];
-			delete $datas["Id_map"];
-			delete $datas["Id_parsel"];
-			socket.emit('hireAgronom', $datas);
+		if(data!=null) {
+			$agronomInfo=JSON.parse();
+			$agronomHireStatus={"Hire_status":$agronomInfo.Hire_status};
+			delete $agronomInfo["Hire_status"];
+			DB.dbUpdateOne("hireAgronom",$agronomInfo,$agronomHireStatus);
+			$datas = DB.dbGetOne("hireAgronom", data);
+
+			if ($datas != null) {
+				delete $datas["_id"];
+				delete $datas["Id_User"];
+				delete $datas["Id_map"];
+				delete $datas["Id_parsel"];
+				socket.emit('hireAgronom', $datas);
+			}
+		}else{
+			socket.emit('hireAgronom', "-1");
 		}
 	});
 }
