9b151f3524294dd045d945cbaa1da6ddeb071210
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-04-06 14:48:41 +0400

    Lobyst

--- modules/agronom.js
+++ modules/agronom.js
@@ -4,7 +4,7 @@ var DB=require("./db.js");
 exports.hireAgronom=function (socket,iosockets){
 	socket.on("hireAgronom",function(data) {
 		if (data != null) {
-			$agronomInfo = JSON.parse();
+			$agronomInfo = JSON.parse(data);
 			$agronomHireStatus = {"Hire_status": $agronomInfo.Hire_status};
 			delete $agronomInfo["Hire_status"];
 			DB.dbUpdateOne("hireAgronom", $agronomInfo, $agronomHireStatus);
