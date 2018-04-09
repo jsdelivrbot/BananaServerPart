7b3ba1ee4b6cdebfdb01ca26464388a6a1afc2dd
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-04-09 13:28:12 +0400

    Lobyst

--- modules/parsel.js
+++ modules/parsel.js
@@ -71,6 +71,7 @@ exports.buyParsel=function (socket,iosockets){
 				delete $datas[$key]["Id_map"];
 			}*/
 			socket.emit('buyParsel', $userData);
+			socket.emit('buyParsel', $dataParsel);
 		}
 	});
 }
