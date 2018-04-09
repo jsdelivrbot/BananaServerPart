7ef97fc128b80ebc85a21734781ebb39776da73d
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-04-09 19:07:00 +0400

    Purge cache

--- modules/parsel.js
+++ modules/parsel.js
@@ -54,9 +54,9 @@ exports.buyParsel=function (socket,iosockets){
 		if(data!=null) {
 
 				DB.getOther(function(res){userInfos=res; return res;},"UserBaseInfo", JSON.stringify($finalDataPay));//DB.dbGetOne("UserBaseInfo", JSON.stringify($finalDataPay));
-				$userData=userInfos;
+				$userData=JSON.parse(userInfos);
 				DB.getOther(function(res){parselinfo=res; return res;},"ParselsBase", JSON.stringify($finZap));
-				$dataParsel=parselinfo;
+				$dataParsel=JSON.parse(parselinfo);
 			$money='{"Money":'+(Number($userData.Money)-Number($dataParsel.Price_Install))+'}';
 			DB.dbUpdateOne("UserBaseInfo",JSON.stringify($finalDataPay),$money);
 			$finalDataPay.Id_parsel              = $dataParsel.Id_parsel;
