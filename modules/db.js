5f3da84e8f78768deff7956bb0bf76275c99cc00
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-04-11 13:17:02 +0400

    Purge cache

--- modules/db.js
+++ modules/db.js
@@ -82,9 +82,7 @@ exports.dbGetOne=function(table,data,db){
 		var collection = datas.collection(table);
 		var infos = JSON.parse(data);
 		console.log(infos);
-		var $res=collection.findOne(infos,function(err,res){
-			return res;
-		});
+		var $res=collection.findOne(infos);
 	//});
 	console.log($res);
 return $res;
