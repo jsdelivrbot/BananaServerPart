ffa3828c72772885d64a01e0661dba8c96111f05
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-04-12 20:19:00 +0400

    Purge cache

--- modules/hire.js
+++ modules/hire.js
@@ -4,20 +4,25 @@ var DB=require("./db.js");
 
 exports.getHire=function (socket,iosockets,db){
 	socket.on("getHire",function(data){
-		$finZap=JSON.parse(data);
-		if($data!=null) {
-			DB.dbSendOne("Hire", $finZap,db);
-			DB.getOtherMore(function(res){
-				if (res != null) {
-					for (var $key in res) {
-						delete res[$key]["_id"];
-						delete res[$key]["Type_employer"];
+		try {
+			$finZap = JSON.parse(data);
+			if ($data != null) {
+				DB.dbSendOne("Hire", $finZap, db);
+				DB.getOtherMore(function (res) {
+					if (res != null) {
+						for (var $key in res) {
+							delete res[$key]["_id"];
+							delete res[$key]["Type_employer"];
+						}
+						socket.emit('getHire', res);
+					} else {
+						socket.emit('getHire', "-1");
 					}
-					socket.emit('getHire', res);
-				}else{
-					socket.emit('getHire', "-1");
-				}},"Hire", data,db);
+				}, "Hire", data, db);
 
+			}
+		}catch(e){
+			socket.emit('getHire', "Invalid request format");
 		}
 	});
 }
