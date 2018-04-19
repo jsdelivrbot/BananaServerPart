b29a3e46ce9ac034005507575a6ab771c6954fcc
Singuliarity1@gmail.com <Singuliarity1@gmail.com>
2018-04-19 15:28:00 +0400

    Purge cache

--- modules/inout.js
+++ modules/inout.js
@@ -1,7 +1,24 @@
 
+function detectedType(data){
+	console.log(data);
+	switch(typeof(data)){
+		case "Number":
+
+			console.log("Number");
+			break;
+		case "String":
+			console.log("Number");
+			break;
+	}
+}
+
 exports.in=function(data){
 	try {
 		infor = JSON.parse(data);
+		$final=infor.length;
+		for(var $i=0;$i<$final;$i++){
+			detectedType(infor[i]);
+		}
 		return infor;
 	}catch(e){
 		return false;
